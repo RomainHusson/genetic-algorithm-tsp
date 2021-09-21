af013bfb0965959e5b0aae725e3d83f9c35751cf
Romain Husson <35651922+RomainHusson@users.noreply.github.com>
2021-09-21 10:57:23 +0200

    Delete files directory

--- files/distancePoints.m
+++
@@ -1,8 +0,0 @@
-function[d]=distancePoints(villes,v1,v2)
-% v1 et v2 sont les indices des villes dont on doit calculer la distance les
-% separant
-xa=villes(1,v1);
-ya=villes(2,v1);
-xb=villes(1,v2);
-yb=villes(2,v2);
-d=sqrt(((xb-xa)^2)+((yb-ya)^2));
